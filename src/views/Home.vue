4912ff380f5017270511e001399618cb7f280e3b
Sébastien Framinet <sebastien.framinet@asdoria.com>
2020-04-27 11:55:19 +0200

    first commit

---
+++ src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="home">
+    <Nav class="mb-6" />
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+import Nav from '@/components/Nav.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld,
+    Nav
+  }
+}
+</script>
